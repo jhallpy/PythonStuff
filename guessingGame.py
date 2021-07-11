8e3e4a155cbe8ea0be0e4e6cdfef94096b747c2c
Joshua Hall <alamoscout117@gmail.com>
2021-07-11 14:06:48 -0400

    added rock paper scissors game and refactored code

--- guessingGame.py
+++ guessingGame.py
@@ -1,25 +1,27 @@
 from random import randrange
 import math
+
 check = True
-cont = ""
 score = 0
+
 while check:
+
     rand = randrange(1,10)
-    print(rand)
     guess = input("Guess the number between 1 and 10! ")
+
     try:
         guess = int(guess)
+
         if(guess == rand):
             print("Good job, you got it right! ")
             score += 1
         else:
             print("You were off by " + str(abs(guess-rand)))
+
         cont = input("Would you like to play again?(y/n) ")
-        if(str.lower(cont)=="y"):
-            check = True
-        else:
-            print("Thank you for playing! Your score was: " + str(score))
+        if(str.lower(cont)!="y"):
             check = False
+            print("Thank you for playing! Your score was: " + str(score))
             
     except ValueError:
         print('Please type a number!')
---
+++ rockpaperscissors.py
@@ -0,0 +1,36 @@
+from random import choice
+
+score = 0
+check = True
+
+while check:
+    rps = ["rock","paper","scissors"]
+    cpu = choice(rps)
+    user = str.lower(input("Rock, Paper, Scissors: "))
+    if user == cpu:
+        print("Tie!")
+    elif user == 'rock' and cpu == 'paper':
+        print('You chose rock versus paper. You lose!')
+    elif user == 'rock' and cpu == 'scissors':
+        print('You chose rock versus scissors. You win!')
+        score+=1
+    elif user == 'paper' and cpu == 'scissors':
+        print('You chose paper versus scissors. You lose!')
+    elif user == 'paper' and cpu == 'rock':
+        print('You chose paper versus rock. You win!')
+        score+=1
+    elif user == 'scissors' and cpu == 'rock':
+        print('You chose scissors versus rock. You lose!')
+    elif user == 'scissors' and cpu == 'paper':
+        print('You chose scissors versus paper. You win!')
+        score+=1
+    else:
+        print('Please choose Rock, Paper, or Scissors next time!')
+        continue
+
+    cont = str.lower(input('Would you like to play again?(y/n) '))
+    if(str.lower(cont)!="y"):
+            check = False
+            print("Thank you for playing! Your score was: " + str(score))
+        
+
