b81c281f85b2b4ae73924b32c68df7ec4ff3ca02
Joshua Hall <alamoscout117@gmail.com>
2021-07-14 15:45:12 -0400

    test PyQt6 program

---
+++ youtubeDownloader.py
@@ -0,0 +1,24 @@
+import sys
+
+from pytube import YouTube
+
+#Import QApplication and all required widgets
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QLabel
+from PyQt6.QtWidgets import QWidget
+
+
+
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle('PyQt6 App')
+window.setGeometry(100,100,280,80)
+window.move(60,15)
+helloMsg = QLabel('<h1> Hello World!</h1>', parent=window)
+helloMsg.move(60,15)
+
+window.show()
+
+sys.exit(app.exec())
